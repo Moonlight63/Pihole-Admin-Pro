3f0cc63fd10e1019ee2d7c699bb3064db57bebf4
Moonlight63 <dalencattmlsp@gmail.com>
2022-03-14 12:44:09 +0000

    Deploying to gh-pages-next from @ Moonlight63/pihole-admin-pro@aef52b53de490e511ed9ceb766f93e63baaf381e 🚀

---
+++ assets/HeroBar.edecabbc.js
@@ -0,0 +1,1 @@
+import{h as o}from"./index.ef9167bf.js";import{o as s,e as a,a as r,w as c,q as n,f as l}from"./vendor.283a5d30.js";const _={class:"p-6 border-t-card border-b-card bg-card"},d={class:"text-3xl font-semibold leading-tight"},h={setup(i){return(e,m)=>{const t=o;return s(),a("section",_,[r(t,null,{default:c(()=>[n("h1",d,[l(e.$slots,"default")])]),_:3})])}}};export{h as _};
